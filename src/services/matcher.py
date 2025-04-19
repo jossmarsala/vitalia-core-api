bde372330a8b1d97bfa7f55e7dccf181428b67df
jossmarsala <marsalahjosefina@gmail.com>
2025-04-18 21:24:00 -0300

    Complete version

--- src/services/matcher.py
+++ src/services/matcher.py
@@ -1,48 +1,79 @@
 # Lógica de matching
 
-from rich import print 
+from rich import print
 from rich.progress import track
 from src.utils.file_helpers import read_json
 from src.config.settings import RESOURCES_JSON
+import time
+
 
 def match_preferences(user: dict) -> list[dict]:
     read_resources = read_json(RESOURCES_JSON)
 
-    article_alimentacion_consciente = [0, read_resources[0]['data']]
-    article_dieta_base_plantas = [0, read_resources[1]['data']]
-    article_ejercicio_movilidad_reducida = [0, read_resources[2]['data']]
-    article_habitos_autocuidado = [0, read_resources[3]['data']]
-    article_integral_trabajo = [0, read_resources[4]['data']]
-    article_manejo_estres = [0, read_resources[5]['data']]
-    article_mente_activa = [0, read_resources[6]['data']]
-    article_nutricion_discapacidad = [0, read_resources[7]['data']]
-    article_pausas_activas = [0, read_resources[8]['data']]
-    article_rutina_nocturna = [0, read_resources[9]['data']]
-    article_salud_mental_discapacidad = [0, read_resources[10]['data']]
-    article_siestas_cortas = [0, read_resources[11]['data']]
-    article_tecnica_15_minutos = [0, read_resources[12]['data']]
-    article_vida_sostenible = [0, read_resources[13]['data']]
-    diet_descanso_reparador = [0, read_resources[14]['data']]
-    diet_dia_ocupado = [0, read_resources[15]['data']]
-    diet_dias_estresantes = [0, read_resources[16]['data']]
-    diet_energia_vitalidad = [0, read_resources[17]['data']]
-    diet_semanal_celiaquia = [0, read_resources[18]['data']]
-    diet_semanal_sin_lacteos = [0, read_resources[19]['data']]
-    diet_semanal_vegetariana = [0, read_resources[20]['data']]
-    diet_weekend_sin_lacteos = [0, read_resources[21]['data']]
-    routine_cardio_funcional_express = [0, read_resources[22]['data']]
-    routine_circuito_funcional_adaptado = [0, read_resources[23]['data']]
-    routine_cuerpo_completo_express = [0, read_resources[24]['data']]
-    routine_ejercicios_mejorar_postura = [0, read_resources[25]['data']]
-    routine_entrenamiento_matutino = [0, read_resources[26]['data']]
-    routine_fortalecer_espalda = [0, read_resources[27]['data']]
-    routine_fuerza_energia = [0, read_resources[28]['data']]
-    routine_hiit_liberar_tension = [0, read_resources[29]['data']]
-    routine_yoga_movilidad_reducida = [0, read_resources[30]['data']]
-    routine_yoga_suave = [0, read_resources[31]['data']]
+    articles = [
+        {"id": "article_alimentacion_consciente", "points": 0},
+        {"id": "article_dieta_base_plantas", "points": 0},
+        {"id": "article_ejercicio_movilidad_reducida", "points": 0},
+        {"id": "article_habitos_autocuidado", "points": 0},
+        {"id": "article_integral_trabajo", "points": 0},
+        {"id": "article_manejo_estres", "points": 0},
+        {"id": "article_mente_activa", "points": 0},
+        {"id": "article_nutricion_discapacidad", "points": 0},
+        {"id": "article_pausas_activas", "points": 0},
+        {"id": "article_rutina_nocturna", "points": 0},
+        {"id": "article_salud_mental_discapacidad", "points": 0},
+        {"id": "article_siestas_cortas", "points": 0},
+        {"id": "article_tecnica_15_minutos", "points": 0},
+        {"id": "article_vida_sostenible", "points": 0},
+        {"id": "diet_descanso_reparador", "points": 0},
+        {"id": "diet_dia_ocupado", "points": 0},
+        {"id": "diet_dias_estresantes", "points": 0},
+        {"id": "diet_energia_vitalidad", "points": 0},
+        {"id": "diet_semanal_celiaquia", "points": 0},
+        {"id": "diet_semanal_sin_lacteos", "points": 0},
+        {"id": "diet_semanal_vegetariana", "points": 0},
+        {"id": "diet_weekend_sin_lacteos", "points": 0},
+        {"id": "routine_cardio_funcional_express", "points": 0},
+        {"id": "routine_circuito_funcional_adaptado", "points": 0},
+        {"id": "routine_cuerpo_completo_express", "points": 0},
+        {"id": "routine_ejercicios_mejorar_postura", "points": 0},
+        {"id": "routine_entrenamiento_matutino", "points": 0},
+        {"id": "routine_fortalecer_espalda", "points": 0},
+        {"id": "routine_fuerza_energia", "points": 0},
+        {"id": "routine_hiit_liberar_tension", "points": 0},
+        {"id": "routine_yoga_movilidad_reducida", "points": 0},
+        {"id": "routine_yoga_suave", "points": 0}
+    ]
 
     for resource in read_resources:
         for key, value in resource["data"].items():
             if key in user["data"] and user["data"][key] == value:
-                print(f"Match! {key} se encuentra en user y vale {value}")
-            
+                for article in articles:
+                    if resource["id"] == article["id"]:
+                        article["points"] += 1
+
+    print("¡Listo! Ahora te mostraremos recursos que podrían ayudarte a alcanzar tus metas de bienestar:")
+    print(articles)
+
+    print("[bold]Artículos para leer:")
+    for article in articles:
+        if article["id"].startswith("article") and article["points"] >= 1:
+            for resource in read_resources:
+                if resource["id"] == article["id"]:
+                    print(f"   - {resource["title"]}")
+
+    print("[bold]Rutinas de ejercicio:")
+    for article in articles:
+        if article["id"].startswith("routine") and article["points"] >= 1:
+            for resource in read_resources:
+                if resource["id"] == article["id"]:
+                    print(f"   - {resource["title"]}")
+
+    print("[bold]Planes alimenticios:")
+    for article in articles:
+        if article["id"].startswith("diet") and article["points"] >= 1:
+            for resource in read_resources:
+                if resource["id"] == article["id"]:
+                    print(f"   - {resource["title"]}")
+
+    time.sleep(25)
--- src/utils/file_helpers.py
+++ src/utils/file_helpers.py
@@ -11,7 +11,6 @@ def read_json(path: str) -> list[dict]:
         write_json(path, [])
         return []
  
-    
 def write_json(path: str, content: list) -> None:
     with open (path, "w") as file:
         json.dump(content, file, indent = 4, ensure_ascii = False)
