7747e3393f5d6a188707acde87310bed86055a83
jossmarsala <marsalahjosefina@gmail.com>
2025-05-15 19:02:53 -0300

    Refactor: Improve code formatting and structure in ScoreService for better readability

--- src/services/score_service.py
+++ src/services/score_service.py
@@ -2,12 +2,14 @@ from typing import List
 import asyncio
 
 from src.exceptions import app_exceptions as ae
+from src.schemas.paginated_schemas import PaginationMeta
 from src.schemas.score_schemas import (
-    NewScoreRequest, 
+    NewScoreRequest,
     UpdateScoreRequest,
-    ScoreResponse, 
+    ScoreResponse,
     ScorePaginatedResponse
 )
+
 
 class ScoreService():
     def __init__(self, score_repo):
@@ -19,35 +21,36 @@ class ScoreService():
             self.__get_score_list(page, limit),
             self.__count()
         )
-        total_pages = (total_count // limit) + (0 if total_count % limit == 0 else 1)
+        total_pages = (total_count // limit) + \
+            (0 if total_count % limit == 0 else 1)
         total_pages = 1 if (page == 1 and total_count == 0) else total_pages
 
         if page > total_pages:
             raise ae.NotFoundError()
 
         return ScorePaginatedResponse(
-            results = score,
-            meta={
-                "current_page": page,
-                "total_pages": total_pages,
-                "total_items": total_count,
-                "items_per_page": limit,
-                "has_previous_page": page < total_pages,
-                "has_next_page": page > 1
-            }
+            results=score,
+            meta= PaginationMeta(
+                current_page=page,
+                total_pages=total_pages,
+                total_items=total_count,
+                items_per_page=limit,
+                has_previous_page=page > 1,
+                has_next_page=page < total_pages
+            )
         )
-    
+
     async def create(self, data: NewScoreRequest) -> ScoreResponse:
         return ScoreResponse(
-            id = 1,
-            planes_alimenticios = data.planes_alimenticios,
-            rutinas = data.rutinas,
-            articulos = data.articulos
+            id=1,
+            planes_alimenticios=data.planes_alimenticios,
+            rutinas=data.rutinas,
+            articulos=data.articulos
         )
-    
+
     async def get_by_id(self, score_id: int) -> ScoreResponse:
         raise ae.NotFoundError(f"El puntaje #{score_id} no existe")
-        
+
     async def update(self, score_id: int, data: UpdateScoreRequest) -> ScoreResponse:
         raise ae.NotFoundError(f"El puntaje #{score_id} no existe")
 
@@ -60,4 +63,4 @@ class ScoreService():
 
     async def __get_score_list(self, page: int, limit: int) -> List[ScoreResponse]:
         # TODO: Llamar al repo y obtener listado
-        return [] 
+        return []
