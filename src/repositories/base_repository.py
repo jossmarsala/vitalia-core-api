57a2040fba019232c705e6dd92a497b384441b6c
jossmarsala <marsalahjosefina@gmail.com>
2025-05-19 19:55:28 -0300

    Refactor: Improve count and get_list methods in BaseRepository; enhance create method logic

--- src/repositories/base_repository.py
+++ src/repositories/base_repository.py
@@ -1,17 +1,29 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List, Any
+from datetime import datetime
 
 class BaseRepository(ABC):
     async def count(self, criteria: dict = {}) -> int:
         data = await self._read_all()
         if criteria:
-            data = [item for item in data if ]
-
+            data = [item for item in data if all(item.get(key) == value for key, value in criteria.items)]
+        return len(data)
+    
     async def get_list(self, page: int, limit: int, criteria: dict = {}) -> list[dict]:
-        pass
+        data = await self._read_all()
+        if criteria:
+            data = [item for item in data if all(item.get(key) == value for key, value in criteria.items)]
+        start = (page - 1) * limit
+        end = start + limit
+        return data[start:end]
 
     async def create(self, data: dict) -> dict:
-        pass
+        data["id"] = await self._get_next_id()
+        data["created_at"] = datetime.now().isoformat()
+        data["updated_at"] = datetime.now().isoformat()
+        db = await self._read_all()
+        db.append(data)
+        await self._update_db(db)
 
     async def get_by_criteria(self, criteria: dict) -> dict | None:
         pass
@@ -24,4 +36,12 @@ class BaseRepository(ABC):
 
     @abstractmethod
     async def _read_all(self) -> List[Dict[str, Any]]:
+        pass
+
+    @abstractmethod
+    async def _get_next_id(self) -> int:
+        pass
+
+    @abstractmethod
+    async def _update_db(self, db: List[Dict[str, Any]]) -> None:
         pass
